86e837a2ab037691c5350ec6f0c9f64149ea94a2
tabularasa066a <tabularasa066a@gmail.com>
2021-10-17 09:10:06 +0000

    5th

--- test01.pl
+++ test01.pl
@@ -1,2 +1,25 @@
 human(taro).
+human(jiro).
+human(hanako).
+human(yosiko).
+
+dog(pochi).
+dog(john).
+dog(nana).
+dog(mimi).
+
+male(taro).
+male(jiro).
+male(pochi).
+male(john).
+
+female(hanako).
+female(yosiko).
+female(nana).
+female(mimi).
+
+marriageble(A,B) :- male(A), female(B).
+marriageble(A,B) :- male(B), female(A).
+
 mortal(A) :- human(A).
+mortal(A) :- dog(A).
